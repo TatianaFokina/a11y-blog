4340265e41f3d4e497b2f304bfd9f7b3d2640a51
TatianaFokina <fokinatatian@gmail.com>
2021-07-04 21:06:41 +0300

    Add .njk templates and includes

---
+++ src/data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"siteLink": "",
+	"title": "Код без барьеров — блог про разработку и цифровую доступность",
+	"description": "Блог веб-разработчицы Татьяны Фокиной о доступной веб-разработке.",
+	"ogImageLink": "https://fokina-dev.ru/assets/og-image.jpg",
+	"currentYear": new Date().getFullYear(),
+};
