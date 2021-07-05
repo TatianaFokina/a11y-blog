d217d66b36327da12f1915409f51fb331995a9e9
TatianaFokina <fokinatatian@gmail.com>
2021-07-05 14:47:34 +0300

    Fix comments

--- src/scripts/focus/keyboard-focus.js
+++ src/scripts/focus/keyboard-focus.js
@@ -1,12 +1,12 @@
 export function KeyboardFocus() {
-	// Adds keyboard-accessible class to <body>
+	// Add keyboard-accessible class to <body>
 	document.addEventListener("keydown", (e) => {
 		if (e.key === "Tab") {
 			document.body.classList.add("keyboard-focus");
 		}
 	});
 
-	// Removes class from <body> in CSS
+	// Remove class from <body> in CSS
 	document.addEventListener("mousedown", () => {
 		document.body.classList.remove("keyboard-focus");
 	});
