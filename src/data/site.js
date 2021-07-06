d4565363c331eddaba618e1ebb1ca402a0e56534
TatianaFokina <fokinatatian@gmail.com>
2021-07-06 03:23:11 +0300

    Rewrite main page title & description

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"siteLink": "https://code-without-barriers.ru",
-	"title": "Код без барьеров — блог про разработку и цифровую доступность",
-	"description": "Блог веб-разработчицы Татьяны Фокиной о доступной веб-разработке.",
+	"title": "Код без барьеров — блог о доступной разработке",
+	"description": "Блог веб-разработчицы Татьяны Фокиной о цифровой доступности.",
 	"ogImageLink": "https://code-without-barriers.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
