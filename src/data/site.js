45c1904ab2c417316b727fe1ab62fcf209009240
TatianaFokina <fokinatatian@gmail.com>
2021-07-18 00:04:21 +0300

    Rename, new OG image

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"siteLink": "https://code-without-barriers.ru",
-	"title": "Код без барьеров — блог о доступной разработке",
-	"description": "Блог веб-разработчицы Татьяны Фокиной о цифровой доступности.",
-	"ogImageLink": "https://code-without-barriers.ru/assets/og-image.jpg",
+	"title": "Блог Татьяны Фокиной",
+	"description": "Блог о доступной разработке.",
+	"ogImageLink": "https://tatiana-fokina-blog.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
