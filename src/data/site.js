7aadc11ca4d1ad2303b04c0427800f4e7700281c
TatianaFokina <fokinatatian@gmail.com>
2021-07-18 14:00:07 +0300

    Change main page description

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"siteLink": "https://tatiana-fokina-blog.ru",
 	"title": "Блог Татьяны Фокиной",
-	"description": "Блог о цифровой доступности.",
+	"description": "Блог о доступной разработке.",
 	"ogImageLink": "https://tatiana-fokina-blog.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
