800972484d1dd6281dac3dc2067b5b9b61f2d114
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2024-04-13 01:39:21 +0400

    Adds lang

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,6 +2,7 @@ module.exports = {
 	"url": "https://tatiana-fokina-blog.ru",
 	"title": "Блог Татьяны Фокиной",
 	"description": "Блог о доступной разработке.",
+	"lang": "ru",
 	"ogImageLink": "https://tatiana-fokina-blog.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
