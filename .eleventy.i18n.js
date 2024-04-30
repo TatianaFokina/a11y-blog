32d5e410713633a59a46d8a2ba7fd60d51fe8720
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2024-04-30 17:34:16 +0400

    Makes translate filter easier

--- .eleventy.i18n.js
+++ .eleventy.i18n.js
@@ -16,7 +16,7 @@ module.exports = eleventyConfig => {
 	eleventyConfig.addFilter("translate", function(key) {		
 		const currentLang = this.ctx.page.lang; // Get the current language from the page context
 		const translations = this.ctx.i18n[currentLang]; // Get translations for the current language
-		return translations ? translations[key] || key : key; // Return translation or key if translation is missing
+		return translations[key] || key; // Return translation or key if translation is missing
 	});
 
 }	
