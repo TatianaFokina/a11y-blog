4d03699c61b6ac040a73e9867b953fc3af6aafbe
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2024-04-30 19:05:16 +0400

    Fixes combining i18n yaml files

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -5,16 +5,21 @@ const yaml = require('js-yaml');
 const i18nDirectory = path.join(__dirname, 'i18n');
 let translations = {};
 
-// Read all files in the i18n directory
 fs.readdirSync(i18nDirectory).forEach(file => {
-  // Check if the file has the extension . yaml or . yml
   if (file.endsWith('.yaml') || file.endsWith('.yml')) {
-    // Чтение и парсинг содержимого файла
     const fileContents = fs.readFileSync(path.join(i18nDirectory, file), 'utf8');
     const parsedContent = yaml.load(fileContents);
 
-    // Combining the contents of the file with a common translation object
-    translations = { ...translations, ...parsedContent };
+    // Объединяем содержимое файла с объектом переводов по каждому языку
+    Object.keys(parsedContent).forEach(lang => {
+      if (!translations[lang]) {
+        translations[lang] = {};
+      }
+      translations[lang] = {
+        ...translations[lang],
+        ...parsedContent[lang]
+      };
+    });
   }
 });
 
