1451e88e2525b56886b4da57221d13080c0c2255
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2024-05-02 19:09:56 +0400

    Adds robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,7 @@ module.exports = function(eleventyConfig) {
 
 	///// Build options
 	eleventyConfig.addPassthroughCopy('src/manifest.json');
+	eleventyConfig.addPassthroughCopy('src/robots.txt');
 	eleventyConfig.addPassthroughCopy("src/fonts");
 	eleventyConfig.addPassthroughCopy("src/scripts");
 	eleventyConfig.addPassthroughCopy({ "src/assets/*.{svg,jpg,png}": "assets" });
