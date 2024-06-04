c4318d9aa7783b4c4ae7aeab5b852d2106d6f403
TatianaFokina <fokinatatian@gmail.com>
2024-06-05 00:23:10 +0400

    Refactored HTML code on article pages

--- .eleventy.shortcodes.js
+++ .eleventy.shortcodes.js
@@ -1,18 +1,13 @@
 module.exports = eleventyConfig => {
 	eleventyConfig.addShortcode("note", function(content) {
 		return `
-			<aside class="note"><span class="note__emoji" aria-hidden="true">📌</span><p class="note__text">${content}</p></aside>
+			<aside class="article__note"><span class="article__note-icon">📌</span><p class="article__note-text">${content}</p></aside>
 		`;
 	});
-
 
 	eleventyConfig.addShortcode("hiddenSpan", function(content) {
 		return `<span aria-hidden="true">${content}</span>`;
 	});
 
-	// currentYear
 	eleventyConfig.addShortcode("currentYear", () => `${new Date().getFullYear()}`);
-
 }
-
-
