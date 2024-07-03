d99e98aa8af2a58067be90dea7f437b7b2209629
TatianaFokina <fokinatatian@gmail.com>
2024-07-03 19:17:04 +0400

    Added new transforms, fixed shortcode for notes

--- .eleventy.shortcodes.js
+++ .eleventy.shortcodes.js
@@ -1,7 +1,7 @@
 module.exports = eleventyConfig => {
 	eleventyConfig.addShortcode("note", function(content) {
 		return `
-			<aside class="article__note"><span class="article__note-icon">📌</span><p class="article__note-text">${content}</p></aside>
+			<aside class="article__note"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="article__note-icon" aria-hidden="true"><path d="M12 6v12"/><path d="M17.196 9 6.804 15"/><path d="m6.804 9 10.392 6"/></svg><p class="article__note-text">${content}</p></aside>
 		`;
 	});
 
--- .eleventy.transforms.js
+++ .eleventy.transforms.js
@@ -1,7 +1,7 @@
 const prettyData = require("pretty-data");
+const { JSDOM } = require("jsdom");
 
 module.exports = eleventyConfig => {
-
 	eleventyConfig.addTransform("xmlmin", function(content, outputPath) {
 		if(outputPath && outputPath.endsWith(".xml")) {
 			let result = prettyData.pd.xmlmin(content);
@@ -10,5 +10,19 @@ module.exports = eleventyConfig => {
 		return content;
 	});
 
-}
+	// Add classes for some elements
+	eleventyConfig.addTransform("addClasses", function(content, outputPath) {
+		if (outputPath && outputPath.endsWith(".html")) {
+			const dom = new JSDOM(content);
+			const document = dom.window.document;
 
+		// Add class to <pre> elements
+		document.querySelectorAll("pre").forEach(element => {
+			element.classList.add("article__code");
+		});
+
+			return dom.serialize();
+		}
+		return content;
+	});
+}
