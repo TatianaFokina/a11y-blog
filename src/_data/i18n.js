19fc71ed1a9d2c7d97c4353d522571c4a17ec605
TatianaFokina <fokinatatian@gmail.com>
2024-07-08 16:56:37 +0400

    Formatted files

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -1,26 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
 
-const i18nDirectory = path.join(__dirname, 'i18n');
+const i18nDirectory = path.join(__dirname, "i18n");
 let translations = {};
 
-fs.readdirSync(i18nDirectory).forEach(file => {
-  if (file.endsWith('.yaml') || file.endsWith('.yml')) {
-    const fileContents = fs.readFileSync(path.join(i18nDirectory, file), 'utf8');
-    const parsedContent = yaml.load(fileContents);
+fs.readdirSync(i18nDirectory).forEach((file) => {
+	if (file.endsWith(".yaml") || file.endsWith(".yml")) {
+		const fileContents = fs.readFileSync(
+			path.join(i18nDirectory, file),
+			"utf8"
+		);
+		const parsedContent = yaml.load(fileContents);
 
-    // Объединяем содержимое файла с объектом переводов по каждому языку
-    Object.keys(parsedContent).forEach(lang => {
-      if (!translations[lang]) {
-        translations[lang] = {};
-      }
-      translations[lang] = {
-        ...translations[lang],
-        ...parsedContent[lang]
-      };
-    });
-  }
+		// Combine content of a file with a translation object for each language
+		Object.keys(parsedContent).forEach((lang) => {
+			if (!translations[lang]) {
+				translations[lang] = {};
+			}
+			translations[lang] = {
+				...translations[lang],
+				...parsedContent[lang],
+			};
+		});
+	}
 });
 
 module.exports = translations;
