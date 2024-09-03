b75fad04777749a1e08102df9d6e93be4e7eadef
TatianaFokina <fokinatatian@gmail.com>
2024-09-04 02:29:56 +0400

    Updates

--- scripts/color-theme/color-theme.js
+++ scripts/color-theme/color-theme.js
@@ -1,29 +1,31 @@
 export function colorTheme() {
-	const bodyClass = document.querySelector('.base');
-	const storageKey = 'theme-preference';
-	const switchers = document.querySelectorAll('.header__switcher-button');
+	const bodyClass = document.querySelector(".base");
+	const storageKey = "theme-preference";
+	const switchers = document.querySelectorAll(".header__switcher-button");
 
 	const getThemePreference = () => {
 		if (localStorage.getItem(storageKey))
-			return localStorage.getItem(storageKey)
+			return localStorage.getItem(storageKey);
 		else
-			return window.matchMedia('(prefers-color-scheme: dark)').matches
-			? 'dark'
-			: 'light';
+			return window.matchMedia("(prefers-color-scheme: dark)").matches
+				? "dark"
+				: "light";
 	};
 
 	// Add a false value for aria-pressed first and after that set true
 	const pressSwither = (clickedSwitcher) => {
 		for (let switcher of switchers) {
-			switcher.setAttribute('aria-pressed', 'false');
-		};
-		clickedSwitcher.setAttribute('aria-pressed', 'true');
+			switcher.setAttribute("aria-pressed", "false");
+		}
+		clickedSwitcher.setAttribute("aria-pressed", "true");
 	};
 
 	// Find a pressed switcher and find the currunt theme value
 	const restore = () => {
 		let savedTheme = getThemePreference();
-		const savedSwitcher = document.querySelector(`.header__switcher-button[value=${savedTheme}]`);
+		const savedSwitcher = document.querySelector(
+			`.header__switcher-button[value=${savedTheme}]`
+		);
 
 		pressSwither(savedSwitcher);
 	};
@@ -35,7 +37,7 @@ export function colorTheme() {
 	};
 
 	const reflectPreference = () => {
-		bodyClass.setAttribute('data-theme', theme.value);
+		bodyClass.setAttribute("data-theme", theme.value);
 	};
 
 	const theme = {
@@ -51,26 +53,23 @@ export function colorTheme() {
 		reflectPreference();
 
 		// Now this script can find and listen for clicks on the control
-		switchers.forEach(
-			(event) => event.addEventListener('click', 
-				() => {
+		switchers.forEach((event) =>
+			event.addEventListener("click", () => {
+				// Set theme current variable
+				theme.value =
+					event.getAttribute("value") === "light" ? "light" : "dark";
 
-					// Set theme current variable
-					theme.value = event.getAttribute('value') === 'light'
-					? 'light'
-					: 'dark';
-
-					pressSwither(event);
-					setPreference();
-				}
-			)
+				pressSwither(event);
+				setPreference();
+			})
 		);
 	};
 
 	// Sync with system changes
 	window
-		.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ( {matches:isDark} ) => {
-  		theme.value = isDark ? 'dark' : 'light';
-  		setPreference();
-	});
+		.matchMedia("(prefers-color-scheme: dark)")
+		.addEventListener("change", ({ matches: isDark }) => {
+			theme.value = isDark ? "dark" : "light";
+			setPreference();
+		});
 }
